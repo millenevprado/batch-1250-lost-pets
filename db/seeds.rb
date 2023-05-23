48a8bc6111caac001ab982f1199e54dc803a2331
Millene Prado <m.prado@researchable.nl>
2023-05-23 18:10:28 -0300

    finish crud, add bootstrap e simple form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Starting seed...'
+
+puts 'Cleaning db...'
+Pet.destroy_all
+
+puts 'Creating pets...'
+
+10.times do
+  Pet.create(
+    name: Faker::Creature::Dog.name,
+    species: Pet::SPECIES.sample,
+    address: Faker::Address.full_address,
+    found_on: Faker::Date.between(from: 10.days.ago, to: Date.today)
+  )
+end
+
+puts "#{Pet.count} pets created!"
+puts 'Finished! :D'
